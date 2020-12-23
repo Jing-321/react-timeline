38de4d6c21c5a4423838c74cd3c6dcdd9962e394
Yingjing Lin <yjlin789@gmail.com>
2020-12-22 23:52:05 -0800

    complete Timeline.js

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
   
-  return;
+  const eventComponents = props.events.map((event, i) => {
+    return(
+      <div key={i}>
+        <TimelineEvent person={event.person} timeStamp={event.timeStamp} status={event.status} />
+      </div>
+    )
+  });
+
+  return (
+    <div className='timeline'>
+      {eventComponents}
+    </div>
+  );
 }
 
 export default Timeline;
